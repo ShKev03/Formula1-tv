507391e0e6072272bf5c79e0ed08cd23be894ef4
jjs006 <88708222+jjs006@users.noreply.github.com>
2021-08-28 12:06:29 +0530

    f1-tv

---
+++ styles/index.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+    background-color: #000;
+    flex-direction: column;
+    padding: 20px;
+`;
+
+export const Image = styled.img`
+    height: 100px;
+    margin: 10px;
+`;
+
+export const IFrame = styled.iframe`
+    border: 3px solid #ffffff15;
+    margin: 20px;
+`;
